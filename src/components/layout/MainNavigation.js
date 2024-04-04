d231ea4f207f72a74919223f004cd7d705c2f9df
moataz-armash <mo3taz314@gmail.com>
2024-04-04 16:09:04 +0300

    make responsive navbar and assign colors to variables

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,28 +1,47 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";    
-import classes from './MainNavigation.module.css';
+import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 import FavoritesContext from "../../store/favorites-context";
-function MainNavigation(){
-    const favoritesCtx = useContext(FavoritesContext);
-    return <header className={classes.header}>
-        <div className={classes.logo}>React Meetups</div>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">All Meetups</Link>
-                </li>
-                <li>
-                    <Link to="/new-meetup">Add New Meetup</Link>
-                </li>
-                <li>
-                    <Link to="/Favorites">
-                        My Favorites
-                        <span className={classes.badge}>{favoritesCtx.totalFavorites}</span>
-                    </Link>
-                </li>
-         
-            </ul>
-        </nav>
-        </header>
+
+function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <input
+        type="checkbox"
+        id="nav-toggle"
+        hidden
+        className={classes.navToggle}
+      />
+      <label htmlFor="nav-toggle" className={classes.navIcon}>
+        <div className={classes.navIconLine}></div>
+      </label>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link to="/" className={classes.navLink}>
+              All Meetups
+            </Link>
+          </li>
+          <li>
+            <Link to="/new-meetup" className={classes.navLink}>
+              Add New Meetup
+            </Link>
+          </li>
+          <li>
+            <Link to="/favorites" className={classes.navLink}>
+              My Favorites
+              <span className={classes.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
+
 export default MainNavigation;
